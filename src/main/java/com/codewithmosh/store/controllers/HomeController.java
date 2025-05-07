c99ca1562f4d09ba038a150576ebe9bd66fc2f24
Angie <akl5sz@virginia.edu>
2025-05-07 14:36:16 -0400

    basic thymeleaf data transfer between controller and view

---
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model){ //model is a container for data, we use it to pass data from a controller to a view
+        model.addAttribute("name", "Angie");
+        return "index"; //removed html because we are now working with a thymeleaf template
+    }  
+    
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "index.html";
+    }   
+}
