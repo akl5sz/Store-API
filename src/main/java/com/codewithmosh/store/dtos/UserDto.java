d9ae5b6637dc509f2b0351f4deb1f940596a8e93
Angie <akl5sz@virginia.edu>
2025-05-08 18:59:12 -0400

    serilization customization examples with @Json annotations

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,25 @@
 package com.codewithmosh.store.dtos;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonIgnore //hide this field from our json obj
+    @JsonProperty("user_id")  //rename property in json obj     
     private Long id;
     private String name;
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL) //to exclude null properties from json obj
+    private String phoneNumber;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
@@ -8,5 +9,6 @@ import com.codewithmosh.store.entities.User;
 @Mapper(componentModel = "spring")  //do this so spring can create beans of this type at runtime
 //when we compile our app, mapstruct will automatically implement this interface
 public interface UserMapper {
+    @Mapping(target="createdAt", expression="java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
