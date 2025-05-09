b50fbcc580e9741735767b2207f6dad05e7f308c
Angie <akl5sz@virginia.edu>
2025-05-08 21:00:52 -0400

    extracting request headers

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,9 @@ public class UserController {
     private final UserMapper userMapper;
     
     @GetMapping
-    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sort){
+    public Iterable<UserDto> getAllUsers(@RequestHeader(required = false, name="x-auth-token") String authToken, //header is not case sensitive
+        @RequestParam(required = false, defaultValue = "", name = "sort") String sort){ 
+        System.out.println(authToken);
         if(!Set.of("name", "email").contains(sort)){
             sort = "name";
         }
